77dd1364ba6f415b9d953411a2dbc5156d752def
Efana Usua <usuaefana@gmail.com>
2019-10-16 08:50:20 +0100

    added constructor, onInput change to detect inputs and onButtonSubmit when detect button is clicked

--- src/App.js
+++ src/App.js
@@ -15,17 +15,26 @@ const particlesOptions = {
           value_area: 1500
         }
       }
-      // line_linked: {
-      //   shadow: {
-      //     enable: true,
-      //     color: "#3CA9D1",
-      //     blur: 5
-      //   }   
-      // }
+     
     }
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,7 +44,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
         
         {/*
         <FaceRecognition />*/}
