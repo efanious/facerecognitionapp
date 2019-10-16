9fe485b08052514e7ea4061d9b02c1712685c21e
Efana Usua <usuaefana@gmail.com>
2019-10-16 08:29:08 +0100

    added react-particles to background

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React, { Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 250,
+        density: {
+          enable: true,
+          value_area: 1500
+        }
+      }
+      // line_linked: {
+      //   shadow: {
+      //     enable: true,
+      //     color: "#3CA9D1",
+      //     blur: 5
+      //   }   
+      // }
+    }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles"
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
-        {/*
         <Rank />
+        <ImageLinkForm />
+        
+        {/*
         <FaceRecognition />*/}
       </div>
     );
